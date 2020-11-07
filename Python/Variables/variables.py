378922cda84a33509405fcec6fe1a154ec3a31a9
husney_bayona <pasantesena2@cnt.com.co>
2020-11-07 15:39:51 -0500

    Variables

---
+++ Python/Variables/variables.py
@@ -0,0 +1,26 @@
+#Python es un lenguaje de tipado debil por lo cual es dinamico
+#y el tipo de dato se lo asigna el valor que contiene la variable
+
+x = 5
+y = 3
+animal = "Perro"
+email = "correo@gmail.com"
+miVar = "Mi variable"
+
+
+
+a1 , b1, c1 = "a", "e", "i"
+
+valor1 = valor2 = valor3 = "Pepito"
+
+
+print(a1,b1,c1)
+print(valor1, valor2, valor3)
+
+#Concatenación
+
+inicio = "Hola"
+final = "Mundo"
+
+print(inicio + " " + final)
+
