9fb7ec848a57a51af6e855d20348c343d84f76e5
husney_bayona <pasantesena2@cnt.com.co>
2020-11-07 15:56:56 -0500

    Tipos de datos

---
+++ Python/Tipos de datos/tipoDatos.py
@@ -0,0 +1,20 @@
+palabra = "string\n"
+palabra2 = 'string'
+palabra3 = """
+esto
+    es
+        un
+        string
+    con
+formato
+"""
+
+print(palabra, palabra2, palabra3)
+
+
+entero = 5 #int
+flotante = 5.1 #float
+complejo = 1j #numero complejo
+
+print(entero, flotante, complejo)
+
