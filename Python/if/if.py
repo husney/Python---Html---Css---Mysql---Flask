74663f59f7e03970f2f2d5ab86cd751e31b7b87d
husney_bayona <pasantesena2@cnt.com.co>
2020-11-09 19:01:55 -0500

    IF

---
+++ Python/if/if.py
@@ -0,0 +1,31 @@
+
+n1 = 3
+n2 = 1
+
+if(n1 > n2):
+    print (str(n1) + " es mayor a " + str(n2))
+elif (n2 > n1):
+    print(str(n2) + " es mayor a " + str(n1))
+elif("B" == "A"):
+    print("Las letras son iguales") 
+#elif
+#elif
+#elif
+#...
+else:
+    print("los números son iguales")
+
+# if de una sola linea
+if (n1 > n2) : print(str(n1) + " es mayor a " + str(n2)) 
+
+#Operador ternario
+print('es verdadero') if 1 > 2 else print('Es falso')
+
+if(5 > 2 and 2 == 2):
+    print("Las dos son verdaderas")
+
+
+if(1 > 100 or 5 > 2):
+    print("Una de las dos condicines es verdadera")
+
+    
