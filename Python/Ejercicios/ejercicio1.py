82f2ba25188d94ceea282a811863a0bc5bb71c9a
husney_bayona <pasantesena2@cnt.com.co>
2020-11-09 19:12:41 -0500

    Ejercicio 1

---
+++ Python/Ejercicios/ejercicio1.py
@@ -0,0 +1,10 @@
+dato = input('Por favor ingrese un dato: ')
+
+lista = ["Hola" , "Mundo", "Esto", "Es", "Una", "Lista", "o", "Array"]
+
+
+if( lista.count(dato)):
+    print("El dato existe en la lista", dato)
+else:
+    print("El datno NO existe en la lista", dato)
+
