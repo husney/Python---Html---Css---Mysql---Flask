aee3ea059df1d865c4e3bd75fbb02be7f6cdcb33
husney_bayona <pasantesena2@cnt.com.co>
2020-11-09 21:01:16 -0500

    Se agregan validadores a la calculadora

--- Python/Ejercicios/calculadora.py
+++ Python/Ejercicios/calculadora.py
@@ -3,15 +3,33 @@ opcion = 1
 while opcion != 0:
     print()
     opcion = input("Por favor ingrese una opción\n1-suma\n2-resta\n3-multiplicación\n4-división\n5-división entera\n6-potenciación\n0-salir\n... :")
-    opcion = int(opcion)
+    try:
+        opcion = int(opcion)
+    except:
+        print("Error opción no valida")
+        continue
 
     n1 = 0
     n2 = 0
     resultado = 0
 
     if(opcion != 0):
-        n1 = int(input("Por favor ingrese el primer valor: "))
-        n2 = int(input("Por favor ingrese el segundo valor: "))
+        boln1 = False
+
+        while not boln1:
+            try:
+                n1 = int(input("Por favor ingrese el primer valor: "))
+                boln1 = True
+            except:
+                print("Valor no valido por favor ingreselo de nuevo")
+
+        boln2 = False
+        while not boln2:
+            try:
+                n2 = int(input("Por favor ingrese el segundo valor: "))
+                boln2 = True
+            except:
+                print("Valor no valido por favor ingreselo de nuevo")        
 
     if(opcion == 1):
         resultado = n1 + n2
