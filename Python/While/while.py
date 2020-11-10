b4d9dd0079b5e96d5e00ad1bea4503d9a9fdee73
husney_bayona <pasantesena2@cnt.com.co>
2020-11-09 21:42:31 -0500

    While

---
+++ Python/While/while.py
@@ -0,0 +1,15 @@
+
+i = 0 # iterador
+
+while i <= 5: # condición   
+    if(i == 3):
+        i += 1
+        continue
+    else:
+        print(i)
+    i += 1
+    
+
+# break rompe el ciclo
+# continue salta una iteración 
+
