f0a3c87ffc6faae5ebfaf5c5b0cae2316734dfc4
husney_bayona <pasantesena2@cnt.com.co>
2020-11-12 21:58:32 -0500

    Generadores

---
+++ Python/Generadores/generadores.py
@@ -0,0 +1,79 @@
+"""
+def generador(limite):
+    n = 1
+    while n <= limite:
+        yield n * 2
+        n+= 1
+
+
+resultados = generador(20)
+
+print(next(resultados))
+print("Codigo.......")
+print("Codigo.......")
+print("Codigo.......")
+print("Codigo.......")
+print("Codigo.......")
+print(next(resultados))
+"""
+
+def funcion(*elementos):
+    for i in (elementos):
+        return i
+
+
+print(funcion(1,2,3,4,5))
+
+print("------------")
+
+
+def generador(*elementos):
+    for elemento in elementos:
+        yield elemento
+
+
+iterable = generador("Valentina", 1,2,4,5,8.212, "Sara Orrego")
+
+
+print(next(iterable))
+print("muchas lineas de código")
+print("muchas lineas de código")
+print("muchas lineas de código")
+print("muchas lineas de código")
+print("muchas lineas de código")
+print("muchas lineas de código")
+print(next(iterable))
+print("muchas lineas de código")
+print(next(iterable))
+print(next(iterable))
+
+print("------------------")
+
+
+def generadorF(*nombres):
+    for nombre in nombres:
+        yield nombre
+
+nombres = generadorF("Valentina", "Sara", "Camila", "Alejandra")
+
+print("----------------")
+
+
+def generador2(*numeros):
+    for i in numeros:
+        yield i , True
+
+
+resultados = generador2(1,2,3,4,5)
+
+print(next(resultados))
+print("Código")
+print(next(resultados))
+print(next(resultados))
+
+
+ 
+
+
+
+
